556a874565e080975216c009c634f8aa1d35ddb9
Vasia <vasialeleka@ukr.net>
2019-06-06 14:35:48 +0300

    add broadcast reciver

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,21 +1,34 @@
 package com.example.myapplication
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    lateinit var intentServise:Intent
+    lateinit var intentServise: Intent
+    lateinit var br: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         buttonStart.setOnClickListener(this)
         buttonStop.setOnClickListener(this)
-        intentServise = Intent(this,MyService::class.java)
+        intentServise = Intent(this, MyService::class.java)
+        br = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                text.text = intent!!.getStringExtra("String")
+            }
+
+        }
+        LocalBroadcastManager.getInstance(applicationContext)
+                .registerReceiver(br, IntentFilter("ExtraData"))
     }
 
     override fun onClick(v: View?) {
@@ -33,6 +46,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private fun startClick() {
         Toast.makeText(applicationContext, "Start", Toast.LENGTH_SHORT).show()
         startService(intentServise)
+
     }
 
 }
--- app/src/main/java/com/example/myapplication/MyService.kt
+++ app/src/main/java/com/example/myapplication/MyService.kt
@@ -4,20 +4,30 @@ import android.app.Service
 import android.content.Intent
 import android.os.Handler
 import android.os.IBinder
+import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
 
 class MyService : Service() {
+    var start = "hello"
+    var finish = "bye"
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d("Servise", "Command start")
+        var intent = Intent("ExtraData")
+        intent.putExtra("String", start)
+        LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(intent)
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
         Log.d("Servise", "Servise stop")
+        var intent = Intent("ExtraData")
+        intent.putExtra("String", finish )
+        LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(intent)
         super.onDestroy()
     }
+
 }
