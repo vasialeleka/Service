90be8581bd5e5f5141729c1382072d8f96d0f1fd
Vasia <vasialeleka@ukr.net>
2019-06-06 11:59:07 +0300

    service example

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,12 +8,14 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var intentServise:Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         buttonStart.setOnClickListener(this)
         buttonStop.setOnClickListener(this)
+        intentServise = Intent(this,MyService::class.java)
     }
 
     override fun onClick(v: View?) {
@@ -24,10 +27,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun stopClick() {
         Toast.makeText(applicationContext, "Stop", Toast.LENGTH_SHORT).show()
+        stopService(intentServise)
     }
 
     private fun startClick() {
         Toast.makeText(applicationContext, "Start", Toast.LENGTH_SHORT).show()
+        startService(intentServise)
     }
 
 }
--- app/src/main/java/com/example/myapplication/MyService.kt
+++ app/src/main/java/com/example/myapplication/MyService.kt
@@ -2,25 +2,22 @@ package com.example.myapplication
 
 import android.app.Service
 import android.content.Intent
+import android.os.Handler
 import android.os.IBinder
 import android.util.Log
 
 class MyService : Service() {
     override fun onBind(intent: Intent?): IBinder? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return null
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d("Servise", "Command start")
-        var i = 0
-        while (true) {
-            Log.d("Servise", "Service work"+i)
-            i++
-        }
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
+        Log.d("Servise", "Servise stop")
         super.onDestroy()
     }
 }
